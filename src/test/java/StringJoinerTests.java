9b19825d38d1b9e17ade77ddc1bd8bd5cfc5cd34
Keomony <kkhun@thoughtworks.com>
2019-07-19 11:54:08 +0100

    [TDD] (Keomony) Refactor StringJoiner test by creating setup()

--- src/test/java/StringJoinerTests.java
+++ src/test/java/StringJoinerTests.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -6,31 +7,36 @@ import java.util.List;
 
 public class StringJoinerTests {
 
+    private StringsJoiner stringsJoiner;
+    private List<String> inputStrings;
+
 //    Join - Joining a list of strings
 //    means creating a single new string by
 //    concatenating the string in list together with a delimiter between them.
 //    For instance, joining the strings {"a", "b”, "c”} on the delimiter ",” would result in the string "a,b,c”.
 //    Note that there is not a leading or trailing comma.
 
+
+    @BeforeEach
+    void setUp() {
+        stringsJoiner = new StringsJoiner();
+        inputStrings = new ArrayList<>();
+    }
+
     @Test
     void shouldJoinIntoEmptyStringWhenListIsEmpty() {
-        StringsJoiner stringsJoiner = new StringsJoiner();
-        List<String> emptyString = new ArrayList<>();
-        emptyString.add("");
+        inputStrings.add("");
 
-        String actual = stringsJoiner.join(emptyString);
+        String actual = stringsJoiner.join(inputStrings);
 
         Assertions.assertEquals("", actual);
     }
 
     @Test
     void shouldJoinIntoAStringWhenListIsOneString() {
-        StringsJoiner stringsJoiner = new StringsJoiner();
-        List<String> aString = new ArrayList<>();
-        aString.add("any string");
+        inputStrings.add("any string");
 
-        String actual = stringsJoiner.join(aString);
-
+        String actual = stringsJoiner.join(inputStrings);
 
         Assertions.assertEquals("any string", actual);
     }
